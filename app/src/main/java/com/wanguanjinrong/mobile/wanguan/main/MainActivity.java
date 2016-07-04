3e1c634ecfb309ca689246e4785ed16d00946f2c
affe <affe@live.cn>
2016-07-04 09:51:50 +0800

    tablayout

--- app/src/main/java/com/wanguanjinrong/mobile/wanguan/main/MainActivity.java
+++ app/src/main/java/com/wanguanjinrong/mobile/wanguan/main/MainActivity.java
@@ -1,14 +1,64 @@
 package com.wanguanjinrong.mobile.wanguan.main;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.wanguanjinrong.mobile.wanguan.R;
+import com.wanguanjinrong.mobile.wanguan.login.DefaultFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+    @BindView(R.id.sliding_tabs)
+    android.support.design.widget.TabLayout tabLayout;
+
+    private SimpleFragmentPagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        pagerAdapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager(), this);
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+    }
+
+    class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+        final int PAGE_COUNT = 3;
+        private String tabTitles[] = new String[]{"tab1","tab2","tab3"};
+        private int layouts[] = new int[]{R.layout.slider_a,R.layout.slider_b,R.layout.slider_c};
+        private Context context;
+
+        public SimpleFragmentPagerAdapter(FragmentManager fm, Context context) {
+            super(fm);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return DefaultFragment.newInstance(layouts[position]);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return tabTitles[position];
+        }
     }
 }
